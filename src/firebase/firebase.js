351059c4ae6388017f3974b908d65ab9ad73df43
Marko Djordjevic <makezi08@gmail.com>
2018-04-03 18:07:14 +1000

    Add firebase initialization

---
+++ src/firebase/firebase.js
@@ -0,0 +1,29 @@
+import * as firebase from 'firebase';
+
+const prodConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
+
+const devConfig = {
+  apiKey: 'AIzaSyBv3Ivlid-jQitcUT5iMw4PoUzgoYuOjZ8',
+  authDomain: 'react-blog-e17d2.firebaseapp.com',
+  databaseURL: 'https://react-blog-e17d2.firebaseio.com',
+  projectId: 'react-blog-e17d2',
+  storageBucket: 'react-blog-e17d2.appspot.com',
+  messagingSenderId: '57233881913'
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth };
