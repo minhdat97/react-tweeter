5be02f2dc85b45e47ed73f3ac242565b7c0bf7e4
Marko Djordjevic <makezi08@gmail.com>
2018-04-04 10:19:22 +1000

    Add protected routes with authorization checks

--- src/components/pages/AccountPage.js
+++ src/components/pages/AccountPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AuthUserContext from '../AuthUserContext';
 import PasswordForgetForm from '../PasswordForgetForm';
 import PasswordChangeForm from '../PasswordChangeForm';
+import withAuthorization from '../withAuthorization';
 
 const AccountPage = () => (
   <AuthUserContext.Consumer>
@@ -15,4 +16,6 @@ const AccountPage = () => (
   </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const authCondition = authUser => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
